bd8568c379970698a1d80c30ed3ba607c372f102
Srikanta309 <139033378+Srikanta309@users.noreply.github.com>
2023-07-21 17:56:38 +0530

    Update approve.py

--- MukeshRobot/modules/approve.py
+++ MukeshRobot/modules/approve.py
@@ -210,7 +210,7 @@ sᴏᴍᴇᴛɪᴍᴇs, ʏᴏᴜ ᴍɪɢʜᴛ ᴛʀᴜsᴛ ᴀ ᴜsᴇʀ ɴᴏ�
 ❍ /approved *:* ʟɪsᴛ ᴀʟʟ ᴀᴘᴘʀᴏᴠᴇᴅ ᴜsᴇʀs.
 ❍ /unapproveall *:* ᴜɴᴀᴘᴘʀᴏᴠᴇ *ᴀʟʟ* ᴜsᴇʀs ɪɴ ᴀ ᴄʜᴀᴛ. ᴛʜɪs ᴄᴀɴɴᴏᴛ ʙᴇ ᴜɴᴅᴏɴᴇ.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [𓆩𝐒𝐫𝐢𝐤𝐚𝐧𝐭𝐚𓆪](@Me_johnny_deep)............☆
 """
 
 APPROVE = DisableAbleCommandHandler("approve", approve)
